2c3aa60799b49d4fd059ad3b71500fd2f30fdb72
Sillai Timea <timeapk@gmail.com>
2020-02-26 17:53:38 +0200

    Ripple effect for touchable views

--- MiwokKotlin/app/src/main/java/com/example/android/miwokkotlin/model/Word.kt
+++ MiwokKotlin/app/src/main/java/com/example/android/miwokkotlin/model/Word.kt
@@ -1,6 +1,6 @@
 package com.example.android.miwokkotlin.model
 
-import androidx.annotation.IntegerRes
+
 
 class Word (var mDefaultTranslation: String,var mMiwokTranslation: String,var srcImage: Int = 0, var srcAudio: Int = 0) {
 
